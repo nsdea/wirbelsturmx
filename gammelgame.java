579a5472413afceb3481c681df8baf92897a2dc3
Felix <67185896+nsde@users.noreply.github.com>
2022-05-31 21:15:19 +0200

    Update gammelgame.java

--- gammelgame.java
+++ gammelgame.java
@@ -1,15 +1,11 @@
-package defpackage;
-
 import basis.Farbe;
 import basis.Fenster;
 import basis.Hilfe;
 import basis.Knopf;
 import basis.Leinwand;
 
-/* renamed from: Spiel3  reason: default package */
-/* loaded from: Breakout.jar:Spiel3.class */
 public class Spiel3 {
-    private Fenster fenster = new Fenster("Ballpraller 1", 600, 500);
+    private Fenster fenster = new Fenster("WirbelGame", 600, 500);
     private Leinwand leinwand = new Leinwand(20.0d, 20.0d, 560.0d, 390.0d);
     private Knopf kEnde = new Knopf("Ende", 450.0d, 430.0d, 130.0d, 50.0d);
     private Ball ball = new Ball(this.leinwand);
@@ -21,8 +17,7 @@ public class Spiel3 {
         this.schieber.lerneBallKennen(this.ball);
     }
 
-    /* renamed from: führeAus  reason: contains not printable characters */
-    public void m0fhreAus() {
+    public void fuehreAus() {
         while (!this.kEnde.wurdeGedrueckt()) {
             Hilfe.kurzePause();
             if (!this.ball.verloren()) {
